26e0e29d6d3eb207ee506352c6e7bedb9008bbe6
Purshottam Patidar <124254617+Purshottampatidar@users.noreply.github.com>
2023-12-22 16:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i=shortHex.length-3, isCapital=false;
+	let output="#";
+	while (i<shortHex.length) {
+		let ascii =shortHex[i].charCodeAt(0);
+		if(ascii>=65 && ascii<=90){
+			isCapital=true;
+		}
+		output+=shortHex[i]+shortHex[i];
+		i++;
+	}
+	return isCapital?output.toUpperCase(): output;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
+
 alert(extendHex(shortHex));
